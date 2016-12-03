d8ec162559faba979e47aba9a69f602ed0f9df33
qiurunze <1256808682@qq.com>
2016-12-03 17:44:50 +0800

    12334

--- jqueryTest/src/jqueryTest/Base64Util.java
+++ jqueryTest/src/jqueryTest/Base64Util.java
@@ -14,9 +14,6 @@ public class Base64Util {
 	public static String decryptBase64(String data) throws IOException{
 		byte[]  resultBytes = new BASE64Decoder().decodeBuffer(data);
 		return  new String(resultBytes);
-		
-		
-		
 	}
 	
 	
