1b7356ce265124a01b953d81564adf528d79a502
qiurunze <1256808682@qq.com>
2016-12-04 16:16:06 +0800

    sdadsa

--- jqueryTest/src/jqueryTest/base64Test.java
+++ jqueryTest/src/jqueryTest/base64Test.java
@@ -68,7 +68,6 @@ public static void main(String[] args) throws IOException {
 	System.out.println("1234566");
 	System.out.println("1234566");
 	System.out.println("1234566");
-	System.out.println("1234566");
 
 	System.out.println("wdefcsefwef");
 	System.out.println("index");
